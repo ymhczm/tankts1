276e45b4a914cb44db8b98b28895d7c21c7a6543
jayan22 <jackie.yang@ingka.ikea.com>
2022-07-23 17:22:26 +0800

    feat: 初始化webview插件

--- dist/web/extension.js
+++ dist/web/extension.js
@@ -42,28 +42,38 @@ var exports = __webpack_exports__;
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = __webpack_require__(1);
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "json2ts" is now active in the web extension host!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand("json2ts.helloWorld", () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
+        const panel = vscode.window.createWebviewPanel("JSON2TS", // Identifies the type of the webview. Used internally
+        "JSON2TS", // Title of the panel displayed to the user
+        vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+        {} // Webview options. More on these later.
+        );
+        console.log(getWebviewContent());
+        panel.webview.html = getWebviewContent();
         vscode.window.showErrorMessage(new Date().toLocaleString());
         vscode.window.showInformationMessage("test world   hello uuu");
     });
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
-// this method is called when your extension is deactivated
+function getWebviewContent() {
+    return `<!DOCTYPE html>
+		<html lang="en">
+		<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>JSON2TS</title>
+		</head>
+		<body>
+				<p>erthngtyhbh</p>
+				<input/>
+				<img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+		</body>
+		</html>`;
+}
 function deactivate() { }
 exports.deactivate = deactivate;
 
